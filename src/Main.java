e5bd0d697127d5dce829813532f7f97e5881d154
madtro <anna.kozitzyna@yandex.ru>
2023-01-10 21:30:11 +0300

    task3

--- src/Main.java
+++ src/Main.java
@@ -9,5 +9,10 @@ public class Main {
         for (int i = 10; i>0; i--) {
             System.out.println(i);
         }
+
+        System.out.println("Task 3");
+        for (int i = 2; i<=17; i=i+2) {
+            System.out.println(i);
+        }
     }
 }
